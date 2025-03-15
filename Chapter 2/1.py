70df66aa39f8450163373bddd66067c4073cf3c6
abbasAliXDev <abbasali.zellesolutions@gmail.com>
2025-03-15 16:57:42 +0500

    operator

--- Chapter 2/1.py
+++ Chapter 2/1.py
@@ -6,4 +6,5 @@ print(a + b) # 10
 # what is variable?
 # understanding with and example will be helpful so we basically use variable for saving a lot of code in one word or a smaller name so will be easier to call anywhere through his name
 # print(a + b) # 10
-#In this a represent 5 while b also represent 5 because there value above is declare 5 so it represent it.
+# In this a represent 5 while b also represent 5 because there value above is declare 5 so it represent it.
+# anoother example is that if a person put some sugar in sugar gar and call sugar gar when needed it mean that person want to use the sugar not the sugar jar so we use jar to save sugar same in programming we use variable to save in one small name or one word
---
+++ Chapter 2/2.py
@@ -0,0 +1,99 @@
+a = 5
+b = 6
+print(a * b) # 30
+print(a - b) # -1
+print(a + b) # 11
+print(a / b) # 0.83333333333334
+
+# arithmetic operator
+
+# assignment operator 
+
+
+# a = 5 mean assign 5 to a mean when tyhping a 5 is called so good
+
+
+# now 
+
+# a is 5 
+
+a += 3 # a is getting 3 plus so 5 + 3 = 8 so a = 8 now
+print(a) # 8
+a -= 2 # a is subtracting 2 so 8 - 2 = a or 6 = a, a = 6 now its 6
+print(a) # 6
+b *= 4  # b is 6 6 * 4 = 24 so b = 24
+print(b) # 24
+
+# print("abbas"*5) # python can multiply string with number easily
+
+# *
+# **
+# ***
+# ****
+# *****
+
+# make a code to print that paramit
+
+numOfStar = 5
+
+print('\n') # it will make a line break
+
+for star in range(1, numOfStar + 1):
+    print("*"*star)
+
+
+# Comparison Operator
+
+# it always return a value in boolean format in will be true or false
+
+# scenerio
+
+# ==
+# >=
+# <=
+# <
+# >
+# !=
+
+
+# print(5 >= a) # false because we work with a while understanding assignment operator so we will make a new variable to get the result true hope it make sense
+
+newA = 5
+print(5 >= newA) # true
+print(5 > 6) # false
+print(5 < 6) # true
+print(5 != 5) # false
+print(5 == 5) # true
+
+# remember in comment i write boolean value at lowercase but it will always be capitalize mean this true like this is wrong in python True in this format is correct for python e hope it make sense same goes for false like this False is correct false is incorrect
+
+
+# Logical Operator 
+
+# Truth table of 'or'
+# or logic in simple word aik side true return karni chahie toh true dega 
+
+print("True or False Mai Ans: " , True or False) #true
+print("False or False Mai Ans: " , False or False) #false
+print("True or True Mai Ans: " , True or True) #true
+print("False or True Mai Ans: " , False or True) #true
+print("5 > 3 or 3 < 3 Mai Ans: " , 5>3 or 3<3) #true
+print("5 < 3 or 3 < 3 Mai Ans: " , 5<3 or 3<3) #false
+
+
+print('\n') # for line break
+
+# Truth table of 'and'
+# and logic in simple word dono side true return karni chahie toh true dega 
+
+print("True and False Mai Ans: " , True and False) #false
+print("False and False Mai Ans: " , False and False) #false
+print("True and True Mai Ans: " , True and True) #true
+print("False and True Mai Ans: " , False and True) #false
+print("5 > 3 and 3 < 3 Mai Ans: " , 5>3 and 3<3) #false
+print("5 < 3 and 3 < 3 Mai Ans: " , 5<3 and 3<3) #false
+
+# now not it convert oppositely mean true to false and simply false to true
+
+print("Using not with True", not(True)) # false
+print("Using not with False", not(False)) # true
