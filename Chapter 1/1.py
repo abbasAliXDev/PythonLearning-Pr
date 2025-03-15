a49463ca9ffc197f193e247db3f0c2bb502f9e11
abbasAliXDev <abbasali.zellesolutions@gmail.com>
2025-03-15 10:56:18 +0500

    third commit

--- Chapter 1/1.py
+++ Chapter 1/1.py
@@ -4,5 +4,31 @@ print("Hello World!")
 
 # print twinkle twinkle little star in python 
 
-print("Twinke Twinke Little Star")
+print('''
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
 
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the trav'ller in the dark,
+Thanks you for your tiny spark,
+He could not see which way to go,
+If you did not twinkle so.
+
+In the dark blue sky you keep,
+And often thro' my curtains peep,
+For you never shut your eye,
+Till the sun is in the sky.
+
+'Tis your bright and tiny spark,
+Lights the trav'ller in the dark:
+Tho' I know not what you are,
+Twinkle, twinkle, little star.
+''')
+
+# use repl and print table of 5 using it 
