a8063174a0d457ce7f4e49e31f9340c016c4a8a3
abbasAliXDev <abbasali.zellesolutions@gmail.com>
2025-03-15 15:50:30 +0500

    new thing

--- Chapter 2/1.py
+++ Chapter 2/1.py
@@ -6,4 +6,4 @@ print(a + b) # 10
 # what is variable?
 # understanding with and example will be helpful so we basically use variable for saving a lot of code in one word or a smaller name so will be easier to call anywhere through his name
 # print(a + b) # 10
-In this a represent 5 while b also represent 5 because there value above is declare 5 so it represent it.
+#In this a represent 5 while b also represent 5 because there value above is declare 5 so it represent it.
